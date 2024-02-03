cccfaf33ab791a12b048b71d1759ed022bf5d27a
Andrew Collins <andrewjoelcollins@gmail.com>
2024-02-03 18:23:24 -0500

    added firebase

--- QuarkClient/app/build.gradle.kts
+++ QuarkClient/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -64,4 +65,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
