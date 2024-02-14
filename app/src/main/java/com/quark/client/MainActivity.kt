a1ee40858517af2a3dc25182d31561095e0346c4
Andrew Collins <andrewjoelcollins@gmail.com>
2024-02-13 21:37:43 -0500

    oopsie broke dev branch

--- app/src/main/java/com/quark/client/MainActivity.kt
+++ app/src/main/java/com/quark/client/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.quark.client.navigation.QuarkNavigation
 import com.quark.client.ui.theme.QuarkClientTheme
 
 class MainActivity : ComponentActivity() {
