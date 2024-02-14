bd1e861f7c22c5b9e8c6d7f57ff05806da3d8d95
LoganCombs04 <logancombs04@icloud.com>
2024-02-13 21:33:01 -0500

    Added a splashscreen on bootup, as well as a logo to the app on the homescreen made by yours truly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/quark/client/MainActivity.kt
+++ app/src/main/java/com/quark/client/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.quark.client.authentication.EmailAuth
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var auth: EmailAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         auth = EmailAuth(Firebase.auth)
         setContent {
