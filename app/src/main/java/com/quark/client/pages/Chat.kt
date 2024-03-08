52856c27367f3509a0e7ae5fd21f212aa82088dd
JayChandiramani <jaychandiramani2003@gmail.com>
2024-03-07 20:50:06 -0500

    when message is sent, lazycolumn scrolls down

--- app/src/main/java/com/quark/client/pages/Chat.kt
+++ app/src/main/java/com/quark/client/pages/Chat.kt
@@ -1,6 +1,5 @@
 package com.quark.client.pages
 
-import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,8 +7,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -18,7 +15,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
@@ -31,7 +27,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -46,20 +41,19 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import com.quark.client.database.Messages
 import com.quark.client.database.QuarkMessage
 import com.quark.client.database.Users
 import com.quark.client.navigation.Screen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
 
 
 data class ChatProps(
@@ -161,9 +155,12 @@ fun CenterAlignedTopAppBar(props: ChatProps) {
                 mutableStateOf("")
             }
 
+            var test by remember {
+                mutableStateOf(0)
+            }
+
             fun sendMessage() {
-                //listState.animateScrollToItem(Int.MAX_VALUE)
-                inputValue = textFieldFocused.toString()
+                test++
             }
 
             LaunchedEffect(key1 = props) {
@@ -171,6 +168,10 @@ fun CenterAlignedTopAppBar(props: ChatProps) {
                 props.user.getUserProfileById(props.uid)?.let { user ->
                     currentUsername = user.username
                 }
+            }
+
+            LaunchedEffect(key1 = test) {
+                listState.animateScrollToItem(messages.size+200)
             }
 
             Column(
