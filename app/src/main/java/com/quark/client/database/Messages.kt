5c99835af958fd0fbc15a17bf40087f3c9f29db4
JayChandiramani <jaychandiramani2003@gmail.com>
2024-03-19 19:46:52 -0400

    added send message function and conversation updates on sender's page

--- app/src/main/java/com/quark/client/database/Messages.kt
+++ app/src/main/java/com/quark/client/database/Messages.kt
@@ -1,6 +1,7 @@
 package com.quark.client.database
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -12,8 +13,10 @@ import kotlin.coroutines.resumeWithException
  * @property firestore the firestore database
  */
 class Messages(
-    private val firestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore,
 ) {
+
+    private val messages = mutableListOf<QuarkMessage>()
     /**
      * Gets all messages to a user, from a user
      * @param conversationID the id of the chat history between users
@@ -27,7 +30,6 @@ class Messages(
 
         subcollectionRef.get()
             .addOnSuccessListener { documents ->
-                val messages = mutableListOf<QuarkMessage>()
                 for (document in documents) {
                     val userFrom = document.getString("userFrom")
                     val body = document.getString("Body")
@@ -36,6 +38,25 @@ class Messages(
                         messages.add(QuarkMessage(userFrom, body))
                     }
                 }
+
+                continuation.resume(messages)
+            }
+            .addOnFailureListener { exception ->
+                continuation.resumeWithException(exception)
+            }
+    }
+
+    suspend fun updateConversation(conversationID: String, userFrom: String, body: String): List<QuarkMessage> = suspendCancellableCoroutine { continuation ->
+        val conversationRef = firestore.collection("messages").document(conversationID)
+        val subcollectionRef = conversationRef.collection("1")
+
+        val newMessage = hashMapOf(
+            "Body" to body,
+            "userFrom" to userFrom,
+        )
+        subcollectionRef.document("Message " + (messages.size + 1).toString()).set(newMessage)
+            .addOnSuccessListener {
+                messages.add(QuarkMessage(userFrom, body))
 
                 continuation.resume(messages)
             }
